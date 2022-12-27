998a7ad2d78e09e51f0fc47aa001d37666bcf29e
Marcus Autenrieth <myself@prakti.org>
2022-12-27 23:14:33 +0100

    ✨ rename length parameter for list validator and support message overrides.

--- lib/dredd/validators/list.ex
+++ lib/dredd/validators/list.ex
@@ -1,8 +1,6 @@
 defmodule Dredd.Validators.List do
   @moduledoc false
 
-  # TODO: 2022-12-27 - Think about renaming length parameters
-
   alias Dredd.{
     Dataset,
     ListErrors
@@ -10,9 +8,9 @@ defmodule Dredd.Validators.List do
 
   @default_message %{
     type: "is not a list",
-    is: "should have %{count} item(s)",
-    min: "should have at least %{count} item(s)",
-    max: "should have at most %{count} item(s)"
+    exact_length: "should have %{count} item(s)",
+    min_length: "should have at least %{count} item(s)",
+    max_length: "should have at most %{count} item(s)"
   }
 
   def call(%Dataset{valid?: false} = dataset, _validator, _opts) do
@@ -46,22 +44,22 @@ defmodule Dredd.Validators.List do
     |> check_length(opts)
   end
 
-  defp check_length(len, %{is: count} = opts) when len != count do
-    message = Map.get(opts, :is_message, @default_message.is)
+  defp check_length(len, %{exact_length: count} = opts) when len != count do
+    message = Map.get(opts, :exact_length_message, @default_message.exact_length)
 
-    {message, %{count: count, kind: :is}}
+    {message, %{count: count, kind: :exact_length}}
   end
 
-  defp check_length(len, %{min: count} = opts) when len < count do
-    message = Map.get(opts, :min_message, @default_message.min)
+  defp check_length(len, %{min_length: count} = opts) when len < count do
+    message = Map.get(opts, :min_length_message, @default_message.min_length)
 
-    {message, %{count: count, kind: :min}}
+    {message, %{count: count, kind: :min_length}}
   end
 
-  defp check_length(len, %{max: count} = opts) when len > count do
-    message = Map.get(opts, :max_message, @default_message.max)
+  defp check_length(len, %{max_length: count} = opts) when len > count do
+    message = Map.get(opts, :max_length_message, @default_message.max_length)
 
-    {message, %{count: count, kind: :max}}
+    {message, %{count: count, kind: :max_length}}
   end
 
   defp check_length(_len, _opts) do
--- test/validators/list_test.exs
+++ test/validators/list_test.exs
@@ -10,8 +10,6 @@ defmodule Dredd.Validators.ListTest do
     SingleError
   }
 
-  # TODO: 2022-12-27 - Test error-message overrides
-
   describe "validate_list" do
     test "adds errors for invalid values in a given list" do
       data = ["string", -1, "string", 0]
@@ -95,7 +93,7 @@ defmodule Dredd.Validators.ListTest do
       end
     end
 
-    property "adds an error if value is a list and does not exactly match `:is`" do
+    property "adds an error if value is a list and does not exactly match `:exact_length`" do
       check all(
               value <- list_of(term(), min_length: 2),
               wrong_length <- positive_integer()
@@ -109,7 +107,7 @@ defmodule Dredd.Validators.ListTest do
             wrong_length
           end
 
-        result = Dredd.validate_list(value, &Dataset.new/1, is: count)
+        result = Dredd.validate_list(value, &Dataset.new/1, exact_length: count)
 
         assert %Dataset{
                  data: ^value,
@@ -119,21 +117,21 @@ defmodule Dredd.Validators.ListTest do
                    message: "should have %{count} item(s)",
                    metadata: %{
                      count: ^count,
-                     kind: :is
+                     kind: :exact_length
                    }
                  }
                } = result
       end
     end
 
-    property "adds an error if value is a list with fewer items than `:min`" do
+    property "adds an error if value is a list with fewer items than `:min_length`" do
       check all(
               value <- list_of(term(), min_length: 1),
               excess_length <- positive_integer()
             ) do
         count = length(value) + 1 + excess_length
 
-        result = Dredd.validate_list(value, &Dataset.new/1, min: count)
+        result = Dredd.validate_list(value, &Dataset.new/1, min_length: count)
 
         assert %Dataset{
                  data: ^value,
@@ -143,14 +141,14 @@ defmodule Dredd.Validators.ListTest do
                    message: "should have at least %{count} item(s)",
                    metadata: %{
                      count: ^count,
-                     kind: :min
+                     kind: :min_length
                    }
                  }
                } = result
       end
     end
 
-    property "adds an error if value is a list with more items than `:max`" do
+    property "adds an error if value is a list with more items than `:max_length`" do
       check all(value <- list_of(term(), min_length: 5)) do
         count = length(value)
 
@@ -161,7 +159,7 @@ defmodule Dredd.Validators.ListTest do
             List.first(Enum.take(integer(1..(count - 2)), 1))
           end
 
-        result = Dredd.validate_list(value, &Dataset.new/1, max: count)
+        result = Dredd.validate_list(value, &Dataset.new/1, max_length: count)
 
         assert %Dataset{
                  data: ^value,
@@ -171,14 +169,14 @@ defmodule Dredd.Validators.ListTest do
                    message: "should have at most %{count} item(s)",
                    metadata: %{
                      count: ^count,
-                     kind: :max
+                     kind: :max_length
                    }
                  }
                } = result
       end
     end
 
-    property "does not add an error if value's length matches `:is` exactly" do
+    property "does not add an error if value's length matches `:exact_length` exactly" do
       check all(value <- list_of(term(), min_length: 2)) do
         count = length(value)
 
@@ -186,11 +184,11 @@ defmodule Dredd.Validators.ListTest do
                  data: ^value,
                  error: nil,
                  valid?: true
-               } = Dredd.validate_list(value, &Dataset.new/1, is: count)
+               } = Dredd.validate_list(value, &Dataset.new/1, exact_length: count)
       end
     end
 
-    property "does not add an error if value has a length greater than `:min`" do
+    property "does not add an error if value has a length greater than `:min_length`" do
       check all(value <- list_of(term(), min_length: 2)) do
         count = length(value)
 
@@ -205,11 +203,11 @@ defmodule Dredd.Validators.ListTest do
                  data: ^value,
                  error: nil,
                  valid?: true
-               } = Dredd.validate_list(value, &Dataset.new/1, min: count)
+               } = Dredd.validate_list(value, &Dataset.new/1, min_length: count)
       end
     end
 
-    property "does not add an error if value has a length less than `:max`" do
+    property "does not add an error if value has a length less than `:max_length`" do
       check all(
               value <- list_of(term(), min_length: 2),
               excess_length <- positive_integer()
@@ -220,7 +218,7 @@ defmodule Dredd.Validators.ListTest do
                  data: ^value,
                  error: nil,
                  valid?: true
-               } = Dredd.validate_list(value, &Dataset.new/1, max: count)
+               } = Dredd.validate_list(value, &Dataset.new/1, max_length: count)
       end
     end
 
@@ -258,6 +256,86 @@ defmodule Dredd.Validators.ListTest do
                  metadata: %{kind: :type}
                }
              } = Dredd.validate_list(data, validator)
+    end
+
+    test "allows setting `type_message`" do
+      data = 100
+      message = "type message"
+
+      validator = fn data ->
+        Dredd.validate_string(data)
+      end
+
+      assert %Dataset{
+               data: ^data,
+               valid?: false,
+               error: %SingleError{
+                 validator: :list,
+                 message: ^message,
+                 metadata: %{kind: :type}
+               }
+             } = Dredd.validate_list(data, validator, type_message: message)
+    end
+
+    test "allows setting `exact_length_message`" do
+      data = ["foo", "foo", "foo"]
+      message = "exact length message"
+
+      validator = fn data ->
+        Dredd.validate_string(data)
+      end
+
+      assert %Dataset{
+               data: ^data,
+               valid?: false,
+               error: %SingleError{
+                 validator: :list,
+                 message: ^message,
+                 metadata: %{kind: :exact_length, count: 10}
+               }
+             } =
+               Dredd.validate_list(data, validator,
+                 exact_length: 10,
+                 exact_length_message: message
+               )
+    end
+
+    test "allows setting `min_length_message`" do
+      data = ["foo", "foo", "foo"]
+      message = "min length message"
+
+      validator = fn data ->
+        Dredd.validate_string(data)
+      end
+
+      assert %Dataset{
+               data: ^data,
+               valid?: false,
+               error: %SingleError{
+                 validator: :list,
+                 message: ^message,
+                 metadata: %{kind: :min_length, count: 10}
+               }
+             } = Dredd.validate_list(data, validator, min_length: 10, min_length_message: message)
+    end
+
+    test "allows setting `max_length_message`" do
+      data = ["This", "is", "way", "too", "large", "Dude!"]
+      message = "max length message"
+
+      validator = fn data ->
+        Dredd.validate_string(data)
+      end
+
+      assert %Dataset{
+               data: ^data,
+               valid?: false,
+               error: %SingleError{
+                 validator: :list,
+                 message: ^message,
+                 metadata: %{kind: :max_length, count: 2}
+               }
+             } = Dredd.validate_list(data, validator, max_length: 2, max_length_message: message)
     end
 
     test "does an early abort if a given dataset is already invalid" do
