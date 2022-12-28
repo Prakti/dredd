21bc8efd2c63dfacb194912229ecf7bfaaa57370
Marcus Autenrieth <myself@prakti.org>
2022-12-28 02:33:06 +0100

    💚 further narrowed range of supported OTP/elixir combinations

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Dredd.MixProject do
       app: :dredd,
       description: "Dredd judges your structs. Validate data with Elixir",
       version: "1.3.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package(),
