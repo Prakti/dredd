3335e44d44f5a4db9fb1e5a5a0192f052e8969f3
Marcus Autenrieth <myself@prakti.org>
2024-07-30 17:29:35 +0200

    💚 fix elixir version.

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Dredd.MixProject do
       app: :dredd,
       description: "Dredd judges your data. Use it to  yalidate arbitrary Elixir data.",
       version: "2.0.2",
-      elixir: "~> 1.16",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package(),
